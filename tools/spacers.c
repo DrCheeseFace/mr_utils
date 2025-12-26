e0c9544b3ebf3cab8f95ec1dd3636b26c449985c
tharun11 <47290042+DrCheeseFace@users.noreply.github.com>
2025-12-26 00:29:24 +0000

    refactor(spacers): removed the goofy ass temp file writing shit

--- tools/spacers.c
+++ tools/spacers.c
@@ -1,33 +1,29 @@
+#include "mr_utils/mrv_vectors.h"
 #define _GNU_SOURCE
-
-#include <mr_utils.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+#include <mr_utils.h>
+
 #define MAX_ARGS 64
 #define MAX_FILES 256
 #define MAX_ARG_LENGTH
-#define VERSION "0.1.0"
+#define VERSION "0.1.1"
 
 typedef enum Option {
 	OPTION_HELP_SHORT,
 	OPTION_HELP,
-
 	OPTION_VERSION_SHORT,
 	OPTION_VERSION,
-
 	OPTION_CHECK_SHORT,
 	OPTION_CHECK,
-
 	OPTION_COUNT,
 } Option;
 
-const char *optionToStr[OPTION_COUNT] = { "-h",	       "--help", "-v",
-					  "--version", "-c",	 "--check" };
-
+const char *option_to_str[OPTION_COUNT] = { "-h",	 "--help", "-v",
+					    "--version", "-c",	   "--check" };
 Bool is_check = FALSE;
 
 void log_help(MrlLogger *logger)
@@ -64,9 +60,8 @@ void log_invalid_arg(MrlLogger *logger, MrsString *arg)
 Err process_arg(MrlLogger *logger, MrsString *arg)
 {
 	Option option = OPTION_COUNT;
-
 	for (int i = 0; i < OPTION_COUNT; i++) {
-		if (strcmp(arg->value, optionToStr[i]) == 0) {
+		if (strcmp(arg->value, option_to_str[i]) == 0) {
 			option = i;
 		}
 	}
@@ -99,120 +94,141 @@ size_t get_trimed_line_length(char *str, int len)
 	mrs_init(len, str, len, &string);
 	mrs_trim_trailing_whitespace(&string);
 	size_t trimed_len = string.len;
-
 	mrs_free(&string);
-
 	return trimed_len;
 }
 
-internal Err write_to_temp(FILE *temp_file, MrsString *line)
+Err write_line_to_file(FILE *file, MrsString *line)
 {
-	size_t written_len =
-		fwrite(line->value, sizeof(char), line->len, temp_file);
+	size_t written_len = fwrite(line->value, sizeof(char), line->len, file);
 	if (written_len != line->len) {
 		return ERR;
 	}
 
-	fwrite("\n", sizeof(char), 1, temp_file);
-
+	fwrite("\n", sizeof(char), 1, file);
 	return OK;
 }
 
-internal Err check_file(FILE *file)
+Err check_file(MrlLogger *logger, FILE *file)
 {
-	Bool end_of_file = FALSE;
-
-	while (end_of_file == FALSE) {
-		char *lineptr = NULL;
-		size_t space_count = CAFE_BABE;
-
-		long len = getline(&lineptr, &space_count, file);
-
-		if (len == EOF) {
-			end_of_file = TRUE;
-		}
-		if (len == -1) {
-			free(lineptr);
-			break;
-		}
-
-		MrsString trimed_line;
-		mrs_init(len, lineptr, len, &trimed_line);
-		mrs_trim_trailing_whitespace(&trimed_line);
-
-		// +1 because no '\n'
-		if (trimed_line.len + 1 != (size_t)len) {
-			mrs_free(&trimed_line);
-			free(lineptr);
-			return ERR;
-		}
-
-		mrs_free(&trimed_line);
-
-		free(lineptr);
-	}
-
-	return OK;
-}
-
-internal Err write_file(MrlLogger *logger, FILE *file)
-{
-	FILE *tmp_file = tmpfile();
+	Bool last_line_is_empty = FALSE;
+	uint line_number = 0;
 
 	for (;;) {
 		char *lineptr = NULL;
 		size_t space_count = CAFE_BABE;
+		ssize_t len = getline(&lineptr, &space_count, file);
 
-		long len = getline(&lineptr, &space_count, file);
-
-		if (len == EOF) {
-			free(lineptr);
-			break;
-		}
 		if (len == -1) {
 			free(lineptr);
 			break;
 		}
 
-		MrsString trimed_line;
-		mrs_init(len, lineptr, len, &trimed_line);
-		mrs_trim_trailing_whitespace(&trimed_line);
+		line_number++;
 
-		Err err = write_to_temp(tmp_file, &trimed_line);
-		if (err != OK) {
+		if (len > 1 && lineptr[len - 2] == ' ') {
+			lineptr[len - 1] = '\0';
 			mrl_logln(logger, MRL_SEVERITY_ERROR,
-				  "couldnt write all elements to a line");
+				  "trailing whitespace found on line %zu: '%s'",
+				  line_number, lineptr);
+			free(lineptr);
 			return ERR;
 		}
 
-		mrs_free(&trimed_line);
+		if (len == 1 && lineptr[0] == '\n') {
+			last_line_is_empty = TRUE;
+		} else {
+			last_line_is_empty = FALSE;
+		}
 
 		free(lineptr);
 	}
 
-	char a;
-	fseek(tmp_file, 0, SEEK_SET);
-	fseek(file, 0, SEEK_SET);
-	while ((a = fgetc(tmp_file)) != EOF) {
-		fputc(a, file);
-	}
-
-	int trunc_err = ftruncate(fileno(file), ftell(tmp_file));
-
-	fclose(tmp_file);
-
-	if (trunc_err != 0) {
+	if (last_line_is_empty) {
+		mrl_logln(logger, MRL_SEVERITY_ERROR,
+			  "extra newlines found at end of file");
 		return ERR;
 	}
 
 	return OK;
 }
 
-internal Err process_files(MrlLogger *logger, MrvVector *file_paths)
+Err write_file(MrlLogger *logger, FILE *file)
+{
+	MrvVector lines;
+	mrv_init(&lines, 1024, sizeof(MrsString));
+
+	for (;;) {
+		char *lineptr = NULL;
+		size_t space_count = 0;
+		ssize_t len = getline(&lineptr, &space_count, file);
+
+		if (len == -1) {
+			free(lineptr);
+			break;
+		}
+
+		MrsString trimed_line;
+		mrs_init(len, lineptr, len, &trimed_line);
+
+		mrs_trim_trailing_whitespace(&trimed_line);
+
+		mrv_append(&lines, &trimed_line);
+		free(lineptr);
+	}
+
+	while (lines.len > 0) {
+		MrsString *last_line = mrv_get_last(&lines);
+
+		if (last_line->len == 0) {
+			mrs_free(last_line);
+			lines.len--;
+		} else {
+			// empty line
+			break;
+		}
+	}
+
+	rewind(file);
+
+	for (size_t i = 0; i < lines.len; i++) {
+		MrsString *line = mrv_get_idx(&lines, i);
+		Err err = write_line_to_file(file, line);
+		if (err != OK) {
+			mrl_logln(logger, MRL_SEVERITY_ERROR,
+				  "couldnt write to file");
+
+			for (size_t j = 0; j < lines.len; j++)
+				mrs_free(mrv_get_idx(&lines, j));
+			mrv_free(&lines);
+
+			return ERR;
+		}
+	}
+
+	fflush(file);
+	int fd = fileno(file);
+	off_t current_pos = ftell(file);
+
+	if (ftruncate(fd, current_pos) != 0) {
+		mrl_logln(logger, MRL_SEVERITY_ERROR,
+			  "could not truncate file");
+		return ERR;
+	}
+
+	for (size_t i = 0; i < lines.len; i++) {
+		mrs_free(mrv_get_idx(&lines, i));
+	}
+
+	mrv_free(&lines);
+
+	return OK;
+}
+
+Err process_files(MrlLogger *logger, MrvVector *file_paths)
 {
 	for (size_t i = 0; i < file_paths->len; i++) {
 		MrsString *file_path = mrv_get_idx(file_paths, i);
-
 		FILE *file = fopen(file_path->value, "r+");
 		if (file == NULL) {
 			mrl_log(logger, MRL_SEVERITY_ERROR,
@@ -220,10 +236,9 @@ internal Err process_files(MrlLogger *logger, MrvVector *file_paths)
 			mrl_logln(logger, MRL_SEVERITY_ERROR, file_path->value);
 			continue;
 		}
-
 		if (is_check == FALSE) {
 			Err err = write_file(logger, file);
-			if (err != OK) {
+			if (err) {
 				mrs_free(file_path);
 				fclose(file);
 				exit(1);
@@ -232,8 +247,8 @@ internal Err process_files(MrlLogger *logger, MrvVector *file_paths)
 					  file_path->value);
 			}
 		} else {
-			Err err = check_file(file);
-			if (err != OK) {
+			Err err = check_file(logger, file);
+			if (err) {
 				mrl_log(logger, MRL_SEVERITY_DEFAULT,
 					file_path->value);
 				mrl_logln(logger, MRL_SEVERITY_ERROR,
@@ -247,7 +262,6 @@ internal Err process_files(MrlLogger *logger, MrvVector *file_paths)
 				mrl_logln(logger, MRL_SEVERITY_OK, " OK");
 			}
 		}
-
 		fclose(file);
 	}
 	return OK;
@@ -256,59 +270,48 @@ internal Err process_files(MrlLogger *logger, MrvVector *file_paths)
 int main(int argc, char **argv)
 {
 	MrlLogger *logger = mrl_create(stderr, TRUE, FALSE);
-
 	MrvVector args;
 	mrv_init(&args, MAX_ARGS, sizeof(MrsString));
-
 	for (size_t i = 0; i < (size_t)argc; i++) {
 		MrsString arg;
 		mrs_init(strlen(argv[i]), argv[i], strlen(argv[i]), &arg);
 		mrs_remove_whitespace(&arg);
 		mrv_append(&args, &arg);
 	}
-
 	if (argc == 1) {
 		log_help(logger);
 	}
-
 	MrvVector file_paths;
 	mrv_init(&file_paths, MAX_FILES, sizeof(MrsString));
-
 	Err err = OK;
 	for (size_t i = 1; i < args.len; i++) {
 		MrsString *arg = mrv_get_idx(&args, i);
-
 		switch (arg->value[0]) {
 		case '-':
 			err = process_arg(logger, arg);
 			break;
 		default:
-			(void)err; // dafak
+			(void)0; // dafak
 			MrsString path;
 			mrs_init(strlen(argv[i]), argv[i], strlen(argv[i]),
 				 &path);
 			mrv_append(&file_paths, &path);
 			break;
 		}
-
 		if (err == ERR) {
 			break;
 		}
 	}
-
 	if (err == OK) {
 		err = process_files(logger, &file_paths);
 	}
-
 	for (size_t i = 0; i < file_paths.len; i++) {
 		mrs_free(mrv_get_idx(&file_paths, i));
 	}
 	mrv_free(&file_paths);
-
 	for (size_t i = 0; i < args.len; i++) {
 		mrs_free(mrv_get_idx(&args, i));
 	}
 	mrv_free(&args);
-
 	mrl_destroy(logger);
 }
